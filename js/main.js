fd159e4ee5a0ac72f64add382410a4d8b3c46014
Yanosh Kunsh <me@yanosh.net>
2021-10-12 10:40:22 +0300

    change the way units are passed and the reponse is handled - use the stdout and stderr for capturing response

--- js/main.js
+++ js/main.js
@@ -1,8 +1,9 @@
+var Module = {};
 var _app = {
 
     // Initialize the app functionality
     init: function () {
-        _app.initCcall();
+        _app.initModule();
         _app.bind();
     },
 
@@ -10,38 +11,64 @@ var _app = {
     bind: function () {
         document.getElementById('main').addEventListener('submit', _app.onMainFormSubmit);
         document.getElementById('copy-result').addEventListener('click', _app.onCopyResultClick);
+        document.addEventListener('runtime-init', _app.onRuntimeInit);
     },
 
-    // Hide the loader once the runtime is initilized and a test call to
-    // covnert_unit was made
-    initCcall: function () {
-        Module.onRuntimeInitialized = async _ => {
-            // Do a first initial calculation, to speedup all subsequent calls
-            var initValue = Module.ccall('convert_unit', 'string', ['string', 'string'], ['1inch', 'm']);
+    // Add initial settins for the WASM module
+    initModule: function () {
+        Module.print = _app.wasmOutput;
+        Module.printErr = _app.wasmOutputError;
 
-            document.getElementById('loader').classList.add('invisible');
+        // Trigger the runtime-init events
+        Module.onRuntimeInitialized = function () {
+            document.dispatchEvent(new CustomEvent('runtime-init'));
         };
+    },
+
+    // Hnadle runtime initilization event
+    onRuntimeInit: function () {
+        // Do a first initial calculation, to speedup all subsequent calls
+        Module.ccall('convert_unit', 'string', ['string', 'string'], ['1inch', 'm']);
+        _app.displayResult('', false);
+
+        // Hide the preloader once the module intializes
+        document.getElementById('loader').classList.add('invisible');
     },
 
     // Handle form calculations
     onMainFormSubmit: function (event) {
-        var have = this['have-value'].value + ' ' + this['have-unit'].value;
-        var result = '';
-
-        try {
-            result = Module.ccall('convert_unit', 'string', ['string', 'string'], [have, this['want-unit'].value]);
-        } catch (e) {
-            result = 'ERR';
-        }
-
-        document.querySelector('#result strong').innerHTML = result;
         event.preventDefault();
+        Module.ccall('convert_unit', 'string', ['string', 'string'], [this['you-have'].value, this['you-want'].value]);
     },
 
     // Handle copy result button click
     onCopyResultClick: function () {
         navigator.clipboard.writeText(document.querySelector('#result strong').innerHTML);
+    },
+
+    // Handle the displaying of calculations
+    displayResult: function (value, isError) {
+        document.querySelector('#result strong').innerHTML = value;
+
+        // Change the style of the result box based on the error status
+        if (isError) {
+            document.getElementById('result').classList.replace('alert-primary', 'alert-danger');
+        } else {
+            document.getElementById('result').classList.replace('alert-danger', 'alert-primary');
+        }
+
+    },
+
+    // Handle the moudule output (this is capturing all the stdout)
+    wasmOutput: function (output) {
+        _app.displayResult(output, false);
+    },
+
+    // Handle the moudule error output (this is capturing all the stderr)
+    wasmOutputError: function (output) {
+        _app.displayResult(output, true);
     }
+
 
 };
 
